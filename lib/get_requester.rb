559591ec46fb0e39f2850a341b3e341987a53aae
Ghostily-Boo <grace.rl.qin@gmail.com>
2020-11-24 18:28:26 -0500

    commit tree c837368ab4ae0336afa4f6b7cfcfb1f3e8be6729

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,19 @@
 # Write your code here
+require 'open-uri'
+require 'pry'
+require 'json'
+
+class GetRequester
+
+    def initialize(url)
+        @url = url
+    end
+
+    def get_response_body
+        URI.parse(@url).open.string
+    end
+
+    def parse_json
+        JSON.load(open(@url))
+    end
+end
